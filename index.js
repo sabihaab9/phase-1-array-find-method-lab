e27dc1e70679f6388822227c102daf4d13b8ef47
Sabiha Bary <sabihaab9@gmail.com>
2022-05-31 12:20:07 -0400

    commit tree b291cf0acaa60ec171a6c686918c83cde92e4b7c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+//Array.prototype.find()
+
+// The find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned. 
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+  ]
+
+
+ function superBowlWin(record){
+  const firstWin = record.find(element => element == 'W');
+  
+ }
+
+
+console.log(firstWin);
